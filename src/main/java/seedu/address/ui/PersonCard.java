a6d89a1e6f9920ffae607821b94e4b1723c2c824
chanyikwai <e0052897@u.nus.edu>
2018-03-07 00:23:30 +0800

    Added Color Coding for Tags

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Person;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+    private static final String[] TAG_COLORS = {"red", "blue", "orange", "green", "yellow", "grey", "white", "black"};
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -47,7 +48,26 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        //person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        initializeTags(person);
+    }
+
+    /*
+     * Returns color for {@code tagName} label
+     */
+    private String getTagColor(String tagName) {
+        return TAG_COLORS[Math.abs(tagName.hashCode()) % TAG_COLORS.length];
+    }
+
+    /*
+     * Create tag labels for {@code person}
+     */
+    private void initializeTags(Person person) {
+        person.getTags().forEach( tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.getStyleClass().add(getTagColor(tag.tagName));
+            tags.getChildren().add(tagLabel);
+        });
     }
 
     @Override
